1e7b26bfaee0be7794cfd6c3142510b707cea709
John Lawrence <johnonrails@gmail.com>
2014-02-24 11:31:37 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WinterOlympics::Application.config.secret_key_base = '13909ee3c3fdc9e8fe304c99600d3a2494117eb27f35844811daca217332df0549ad11ba7981da79e2d1bfb639627b43cc9704e19d2f5e0aa7a05a953c67d070'
